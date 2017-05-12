07732fee856f0305259bd1905df686b34f2f0247
gitpangshun007 <pangshun007@126.com>
2017-05-12 17:18:59 +0800

    修改2

--- public/js/skin.js
+++ public/js/skin.js
@@ -3,6 +3,8 @@
           $li.click(function(){
             $("#"+this.id).addClass("select").
             siblings().removeClass("select");
-            $("#cssfiles").attr("href","public/css/theme/"+this.id+".css");
+            $("#cssfiles").attr("href","https://raw.githubusercontent.com/gitpangshun007/V2EX/master/public/css/theme/"+this.id+".css");
           });
        });
+
+ https://raw.githubusercontent.com/gitpangshun007/V2EX/master/public/css/theme/skin_0.css
